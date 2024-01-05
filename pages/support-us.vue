f402326a0f8b247764d5a3418a2e0ad4eb20c5ce
d4vss <godflod@gmx.at>
2024-01-05 22:28:25 +0100

    Modified Forza Mods Gifs

--- pages/support-us.vue
+++ pages/support-us.vue
@@ -4,7 +4,7 @@
       <div class="flex min-h-[50vh] flex-col lg:justify-around items-center">
         <h1 class="text-4xl lg:text-7xl font-semibold italic underline decoration-primary decoration-4">Support us</h1>
         <div class="flex items-center my-20 flex-col-reverse lg:flex-row gap-20">
-          <img src="/a450770bc2.gif" alt="">
+          <img src="/forza-mods-big.gif" alt="">
           <div>
             <span class="font-medium text-lg">Support us</span>
             <div class="flex flex-col items-start">
