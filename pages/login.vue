9a9a9fcc2debf31e248276c3644333f00bfd7a77
d4vss <godflod@gmx.at>
2024-01-10 22:33:16 +0100

    Removed "Recdirecting..."

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,4 @@
 <template>
-    <div class="min-h-[80vh] flex justify-center items-center">
-        <h1 class="italic underline decoration-primary decoration-4">Redirecting...</h1>
-    </div>
 </template>
 
 <script setup>
