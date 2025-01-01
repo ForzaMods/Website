4f369e44682ee322e8db141058cb2dccf11989e5
d4vss <81194615+d4vss@users.noreply.github.com>
2025-01-01 03:16:27 +0100

    prerender issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,13 @@
-"use client"
-
-import { ArrowDownToLineIcon, ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { css } from "styled-system/css";
+import LatestReleaseButton from "~/components/latest-release-button";
 import NumberTicker from "~/components/number-ticker";
 import { Button } from "~/components/ui/button";
 import { Heading } from "~/components/ui/heading";
 import { Text } from "~/components/ui/text";
 
 export default function Home() {
-  const [selectedValue, setSelectedValue] = useState<any | undefined>();
-
-  useEffect(() => {
-    const fetchTags = async () => {
-      try {
-        const url = "https://api.github.com/repos/ForzaMods/Forza-Mods-AIO/releases";
-        const response = await fetch(url);
-        const data = await response.json();
-        
-        setSelectedValue(data[0].assets[0].browser_download_url);
-      } catch (error) {
-        console.error("Error fetching tags:", error);
-      }
-    };
-  
-    fetchTags();
-  }, []);
   return (
     <>
       <div className={css({
@@ -45,12 +26,7 @@ export default function Home() {
           Forza Mods is a community enabling seamless modding and enhancements for an unparalleled Forza gaming experience.
         </Text>
         <div className={css({ display: "flex", gap: 4, marginTop: 4, flexDirection: { base: "row", mdDown: "column" } })}>
-          <Link href={selectedValue}>
-            <Button variant="solid">
-              Download Latest AIO 
-              <ArrowDownToLineIcon />
-            </Button>
-          </Link>
+          <LatestReleaseButton />
           <Link href="/mods-and-tools">
             <Button variant="subtle" colorPalette="white">
               View all Mods & Tools 
---
+++ src/components/latest-release-button.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { ArrowDownToLineIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function LatestReleaseButton() {
+  const [selectedValue, setSelectedValue] = useState<string | undefined | null>(null);
+  
+    useEffect(() => {
+      const fetchLatestRelease = async () => {
+        try {
+          const url = "https://api.github.com/repos/ForzaMods/Forza-Mods-AIO/releases";
+          const response = await fetch(url);
+          const data = await response.json();
+          
+          setSelectedValue(data[0].assets[0].browser_download_url);
+        } catch (error) {
+          console.error("Error fetching release:", error);
+        }
+      };
+    
+      fetchLatestRelease();
+    }, []);
+
+  return (
+    <Link href={selectedValue || "/"} passHref>
+      <Button variant="solid">
+        Download Latest AIO 
+        <ArrowDownToLineIcon />
+      </Button>
+    </Link>
+  );
+}
